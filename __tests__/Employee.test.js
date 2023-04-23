4990ca1fa4beee32369d9f1764bc040df1855743
meiliou <83768277+meiliou@users.noreply.github.com>
2023-04-23 19:13:28 +0400

    Employee parent created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('name','id', 'email');
+
+    expect(employee.name).toBe('name');
+    expect(employee.id).toBe('id');
+    expect(employee.email).toBe('email');
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('name','id','email');
+    employee.name = 'name';
+
+    expect(employee.getName()).toBe('name');
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('name','id','email');
+    employee.id = 'id';
+
+    expect(employee.getID()).toBe('id');
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('name','id','email');
+    employee.email = 'email';
+
+    expect(employee.getEmail()).toBe('email');
+});
